ab6050fbbc6209ad55acc81af645fecc8e595ed1
Sam Haskell <samhaskell@proton.me>
2023-11-23 00:20:17 +0000

    added basic vector structs w/o functionality

--- src/maths/vector.hpp
+++ src/maths/vector.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
-namespace Ace {
+#include "core/defines.hpp"
 
+namespace Ace {
+    struct Vec2 {
+        f32 x;
+        f32 y;
+    };
+
+    struct Vec3 {
+        f32 x;
+        f32 y;
+        f32 z;
+    };
+
+    struct Vec4 {
+        f32 x;
+        f32 y;
+        f32 z;
+    };
 };
